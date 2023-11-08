502f58722fb56e35da8e3b93bc860365e6183cbf
Microsoft N <pcmci-tech@PCMCI-TECHs-MacBook-Pro.local>
2023-11-09 00:49:02 +0500

    saa

---
+++ src/page/internet/Internet.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { INTERNETS } from "../../static";
+
+function Internet() {
+  const params = useParams();
+  const oneItem = INTERNETS?.find(({ depId }) => depId === params.id);
+  return (
+    <div>
+      <h2>Internet</h2>
+      <table border={1} >
+        <tbody>
+          <td>nomi</td>
+          <td>narxi</td>
+          <td>usd</td>
+          <td>sorov</td>
+        </tbody>
+        {oneItem?.items?.map((i, inx) => (
+          <tr>
+            <td>{i.title}</td>
+            <td>{i.price}</td>
+            <td>{i.usd}</td>
+            <td><a href="tel:*100#">sorov</a></td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default Internet;
--- src/static/index.js
+++ src/static/index.js
@@ -1,104 +1,110 @@
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import SmsIcon from '@mui/icons-material/Sms';
-import SimCardIcon from '@mui/icons-material/SimCard';
-import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SmsIcon from "@mui/icons-material/Sms";
+import SimCardIcon from "@mui/icons-material/SimCard";
+import LanguageIcon from "@mui/icons-material/Language";
 
 export const INNER_ROUTS = [
-    {
-        title: "Internet paketlar",
-        path: "/internet",
-        icon: <LanguageIcon/>
-    },
-    {
-        title: "Usd kodlar",
-        path: "/ussd",
-        icon: "#"
-    },
-    {
-        title: "Tarif rejalari",
-        path: "/tarif",
-        icon: <SimCardIcon/>
-    },
-    {
-        title: "SMS toplamlar",
-        path: "/sms",
-        icon: <SmsIcon/>
-    },
-    {
-        title: "Daqiqalar",
-        path: "/minutes",
-        icon: <AccessTimeIcon/>
-    },
-    {
-        title: "Xizmatlar",
-        path: "/servises",
-        icon: <SettingsIcon/>
-    },
-]
+  {
+    title: "Internet paketlar",
+    path: "/internet",
+    icon: <LanguageIcon />,
+  },
+  {
+    title: "Usd kodlar",
+    path: "/ussd",
+    icon: "#",
+  },
+  {
+    title: "Tarif rejalari",
+    path: "/tarif",
+    icon: <SimCardIcon />,
+  },
+  {
+    title: "SMS toplamlar",
+    path: "/sms",
+    icon: <SmsIcon />,
+  },
+  {
+    title: "Daqiqalar",
+    path: "/minutes",
+    icon: <AccessTimeIcon />,
+  },
+  {
+    title: "Xizmatlar",
+    path: "/servises",
+    icon: <SettingsIcon />,
+  },
+];
 
 export const DATA = [
-    {
-        id: "provider-1",
-        title: "Ucell",
-        path: "ucell"
-    },
-    {
-        id: "provider-2",
-        title: "Beeline",
-        path: "beeline"
-    },
-    {
-        id: "provider-3",
-        title: "Mobiuz",
-        path: "mobiuz"
-    },
-    {
-        id: "provider-4",
-        title: "Uzmobile",
-        path: "uzmobile"
-    },
-]
+  {
+    id: "provider-1",
+    title: "Ucell",
+    path: "ucell",
+  },
+  {
+    id: "provider-2",
+    title: "Beeline",
+    path: "beeline",
+  },
+  {
+    id: "provider-3",
+    title: "Mobiuz",
+    path: "mobiuz",
+  },
+  {
+    id: "provider-4",
+    title: "Uzmobile",
+    path: "uzmobile",
+  },
+];
 
 export const INTERNETS = [
-    {
-        id: "internet-1",
-        depId: "provider-1",
-        items: [
-            {
-                title: "1GB",
-                price: 10_000,
-            },
-        ]
-    },
-    {
-        id: "internet-2",
-        depId: "provider-2",
-        items: [
-            {
-                title: "1GB",
-                price: 9000,
-            },
-        ]
-    },
-    {
-        id: "internet-3",
-        depId: "provider-3",
-        items: [
-            {
-                title: "1GB",
-                price: 8000,
-            },
-        ]
-    },
-    {
-        id: "internet-4",
-        depId: "provider-4",
-        items: [
-            {
-                title: "1GB",
-                price: 11_000,
-            },
-        ]
-    },
-]
+  {
+    id: "internet-1",
+    depId: "provider-1",
+    items: [
+      {
+        title: "1000mb",
+        price: 8900,
+        usd:'57627utyu'
+      },
+      {
+        title: "1300mb",
+        price: 13000,
+        usd:'57627utyu'
+      },
+    ],
+  },
+  {
+    id: "internet-2",
+    depId: "provider-2",
+    items: [
+      {
+        title: "1GB",
+        price: 9000,
+      },
+    ],
+  },
+  {
+    id: "internet-3",
+    depId: "provider-3",
+    items: [
+      {
+        title: "1GB",
+        price: 8000,
+      },
+    ],
+  },
+  {
+    id: "internet-4",
+    depId: "provider-4",
+    items: [
+      {
+        title: "1GB",
+        price: 11_000,
+      },
+    ],
+  },
+];
